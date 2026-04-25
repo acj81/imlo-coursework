a0879c9819ae729b971e6dc4c40ddb0a5e9ff763
acj81 <a_c_jones@tutanota.com>
2026-04-25 13:51:59 +0100

    update train.py

--- train.py
+++ train.py
@@ -119,11 +119,11 @@ class ArchimedesNet(nn.Module):
         # define our actual architecture:
         self.layers = nn.Sequential(
             # feature detection using CNNs:
-            ANBlock(3, 32, 5, 2),
-            ANBlock(32, 32, 4, 2),
-            ANBlock(32, 64, 3, 2),
+            ANBlock(3, 32, 7, 2),
+            ANBlock(32, 32, 5, 2),
+            ANBlock(32, 64, 4, 2),
             nn.BatchNorm2d(64),
-            ANBlock(64, 64, 3, 2),
+            ANBlock(64, 64, 4, 2),
             ANBlock(64, 128, 3, 2),
             ANBlock(128, 128, 2, 2),
             ANBlock(128, 256, 2, 2),
@@ -131,9 +131,11 @@ class ArchimedesNet(nn.Module):
             # image classification using features we detected:
             nn.Flatten(),
             nn.BatchNorm1d(256),
-            nn.Dropout(0.2),
+            nn.Dropout(0.3),
             nn.Linear(256, 512),
             nn.Dropout(0.2),
+            nn.Linear(512, 512),
+            nn.Dropout(0.1),
             nn.Linear(512, 216),
             nn.Dropout(0.1),
             nn.Linear(216, 37),
