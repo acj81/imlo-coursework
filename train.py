766c7a832506737cbe860c2c64febe89156fd42f
acj81 <a_c_jones@tutanota.com>
2026-04-26 19:51:40 +0100

    update train.py

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ class ANDenseBlock(nn.Module):
         for i in range(conv_layers):
             layers += nn.Sequential(
                 nn.Conv2d(in_channels + (growth_rate * i), growth_rate, filter_size, padding="same"),
-                nn.BatchNorm2d(in_channels + (growth_rate * i)),
+                nn.BatchNorm2d(growth_rate),
                 nn.ReLU(),
             )
 
