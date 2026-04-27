a3a8558b92cd0ecffe92e029ae18bf0495f7315f
acj81 <a_c_jones@tutanota.com>
2026-04-27 13:42:42 +0100

    update train.py

--- train.py
+++ train.py
@@ -238,6 +238,7 @@ class ArchimedesNetV3(nn.Module):
             # finally, linear classification:
             nn.Flatten(),
             nn.Linear(1024, 512),
+            nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(512, 37)
         )
