12585d57fc0ff423c525095d6c5528b122c8ddc3
acj81 <a_c_jones@tutanota.com>
2026-04-28 10:22:09 +0100

    update train.py

--- train.py
+++ train.py
@@ -431,14 +431,14 @@ class ArchimedesNetV13(nn.Module):
             # convolution to extract features
             nn.Conv2d(3, 6, 1),
             # dense-trans block combos:
-            ANDense(6, conv_layers=6, growth_rate=32),
-            ANTrans(198, 49),
-            ANDense(49, conv_layers=12, growth_rate=32),
-            ANTrans(433, 109),
-            ANDense(109, conv_layers=30, growth_rate=32),
-            ANTrans(1069, 267),
-            ANDense(267, conv_layers=6, growth_rate=32),
-            ANTrans(459, 114),
+            ANDenseBlock(6, conv_layers=6, growth_rate=32),
+            ANTransBlock(198, 49),
+            ANDenseBlock(49, conv_layers=12, growth_rate=32),
+            ANTransBlock(433, 109),
+            ANDenseBlock(109, conv_layers=30, growth_rate=32),
+            ANTransBlock(1069, 267),
+            ANDenseBlock(267, conv_layers=6, growth_rate=32),
+            ANTransBlock(459, 114),
             # final batch norm, avg pool to reduce features:
             nn.BatchNorm2d(114),
             nn.AvgPool2d(2),
