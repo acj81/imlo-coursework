4fc3a8b8145716d3166b17207f8f06a24cf297b5
acj81 <a_c_jones@tutanota.com>
2026-04-29 09:45:46 +0100

    update train.py

--- train.py
+++ train.py
@@ -716,7 +716,7 @@ class ArchimedesNetV19(nn.Module):
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using accelerator: {device}")
 
-model = ArchimedesNetV18().to(device)
+model = ArchimedesNetV19().to(device)
 
 
 # --- DEFINE OUR TRAIN, TEST AND DATA AUGMENTATION FUNCTIONS ---
