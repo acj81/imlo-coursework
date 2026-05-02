f9ad55ec8866da7b581b23057a986951d0df2936
acj81 <a_c_jones@tutanota.com>
2026-05-02 08:18:26 +0100

    update train.py

--- train.py
+++ train.py
@@ -492,13 +492,13 @@ print(f"Using accelerator: {device}")
 
 model = VisionTransformer(
     image_size = 256, 
-    patch_size = 16, 
+    patch_size = 14, 
     num_channels = 3, 
-    embed_dim = 768, 
+    embed_dim = 1408, 
     num_heads = 8, 
-    num_layers = 12, 
+    num_layers = 48, 
     num_classes = 37, 
-    dropout = 0.0,
+    dropout = 0.1,
 ).to(device)
 
 
