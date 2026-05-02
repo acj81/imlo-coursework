fe9b3b2ad898910d4a34b8affa2dd640b60089f3
acj81 <a_c_jones@tutanota.com>
2026-05-02 12:46:06 +0100

    update train.py

--- train.py
+++ train.py
@@ -156,11 +156,10 @@ device = torch.accelerator.current_accelerator().type if torch.accelerator.is_av
 print(f"Using accelerator: {device}")
 
 
-model = ArchimedesNetV12().to(device)
+#model = ArchimedesNetV12().to(device)
 
 # lot of params for ViT so specify here:
 
-'''
 model = VisionTransformer(
     image_size = 256, 
     patch_size = 16, 
@@ -171,7 +170,6 @@ model = VisionTransformer(
     num_classes = 37, 
     dropout = 0.0,
 ).to(device)
-'''
 
 
 # --- DEFINE OUR TRAIN, TEST AND DATA AUGMENTATION FUNCTIONS ---
