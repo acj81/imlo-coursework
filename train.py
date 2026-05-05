de3af76a21b51207bf0d40479d65974fb2651ce5
acj81 <a_c_jones@tutanota.com>
2026-05-05 13:02:52 +0100

    update train.py

--- train.py
+++ train.py
@@ -186,54 +186,55 @@ class ResNet9(nn.Module):
         return x
 
 
-class ResNet9(nn.Module):
+class ResNet18(nn.Module):
     def __init__(self):
         super().__init__()
         
         # architecture here:
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, 3),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
-        self.res1 = ResBlock(128)
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
-        self.res2 = ResBlock(256)
-        self.res3 = ResBlock(256)
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 512, 3),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
+        self.stem = nn.Conv2d(3, 64, kernel_size=7)
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.conv1 = nn.Conv2d(64, 128, 1)
+        self.conv2 = nn.Conv2d(128, 256, 1)
+        self.conv3 = nn.Conv2d(256, 512, 1)
+        self.conv4 = nn.Conv2d(512, 1024, 1)
+
+        self.res1 = ResBlock(64)
+        self.res2 = ResBlock(64)
+        self.res3 = ResBlock(128)
+        self.res4 = ResBlock(128)
+        self.res5 = ResBlock(256)
+        self.res6 = ResBlock(256)
+        self.res7 = ResBlock(512)
+        self.res8 = ResBlock(512)
+        
         self.lc = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1,1)),
+            nn.AdaptiveAvgNorm2d((1,1)),
             nn.Flatten(),
-            nn.Dropout(0.2),
-            nn.Linear(512, 37),
+            nn.Linear(1024, 37)
         )
 
 
     def forward(self, x):
         # pass through each layer
+        x = self.stem(x)
+        x += self.res1(x)
+        x += self.res2(x)
         x = self.conv1(x)
+        x = self.pool(x)
+        x += self.res3(x)
+        x += self.res4(x)
         x = self.conv2(x)
-        x = self.res1(x)
+        x = self.pool(x)
+        x += self.res5(x)
+        x += self.res6(x)
         x = self.conv3(x)
-        x = self.res2(x)
-        x = self.res3(x)
+        x = self.pool(x)
+        x += self.res7(x)
+        x += self.res8(x)
         x = self.conv4(x)
+        x = self.pool(x)
         x = self.lc(x)
         return x
 
